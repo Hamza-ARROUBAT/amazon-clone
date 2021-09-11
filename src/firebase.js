d2c9bdb440d310ed48ec876dd46fceb8f6dba5ed
Hamza ARROUBAT <ht.arroubat@gmail.com>
2021-09-11 09:35:41 +0100

    done design

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5sD0mtEvUz_xgfBCU9H0x0Zf50x_3_PE",
+    authDomain: "clone-fb940.firebaseapp.com",
+    projectId: "clone-fb940",
+    storageBucket: "clone-fb940.appspot.com",
+    messagingSenderId: "295762543507",
+    appId: "1:295762543507:web:83d4863ca3c36d98395e66"
+  };
